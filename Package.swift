4ac76b8ad6ee43fdb4a262279788ecce0bd56661
Casey Eastman <casey@datasproductions.com>
2016-10-16 13:51:32 -0600

    updates package dependecy url

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "NetworkLayer",
     dependencies:[
-        .Package(url: "https://github.com/BigCase/Alamofire.git", "4.0.2")
+        .Package(url: "https://github.com/BigCase/Alamofirez.git", "4.0.2")
     ]
 )
