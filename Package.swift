0c58d1e34da57661b542bef07e7345276b98e22d
Casey Eastman <casey@datasproductions.com>
2016-10-16 13:35:09 -0600

    fix tests module name

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "NetworkLayer",
     dependencies:[
-        .Package(url: "https://github.com/BigCase/Alamofire.git", Version(4,0,1))
+        .Package(url: "https://github.com/BigCase/Alamofire.git", Version(4,0,2))
     ]
 )
