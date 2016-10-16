474b5e121f5e4980fe7697d075b802cdbda07892
Casey Eastman <casey@datasproductions.com>
2016-10-16 01:19:54 -0600

    update package

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "NetworkLayer",
     dependencies:[
-        .Package(url: "https://github.com/Alamofire/Alamofire.git", Version(4,0,1))
+    .Package(url: "https://github.com/Alamofire/Alamofire.git", Version(3,5,1))
     ]
 )
